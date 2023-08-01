2bd1a0a8dfa3cc2783cdbcf9612388351bf90ed3
Virendra-Verma <vkverma991099@gmail.com>
2023-08-01 18:58:25 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import './App.css';
-import Navbar from './Navbar';
-import TextForm from './TextForm';
-import React,{ useState } from 'react';
-
+import "./App.css";
+import Navbar from "./Navbar";
+import TextForm from "./TextForm";
+import About from "./About";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
-  const [mode,setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
-  const tleMode=()=>{
-    if(mode==='light'){
-     setMode('dark');
-      document.body.style.backgroundColor='#0d1829';
-    }else{
-     setMode('light');
-      document.body.style.backgroundColor='white';
+  const tleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#0d1829";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
     }
-  }
+  };
   return (
     <>
-    <Navbar title="TextUtils" mode={mode} tleMode={tleMode}/>
-    <div className='container my-3'>
-    <TextForm heading="Enter the text to analyze below" mode={mode}/>
-    {/* <About/> */}
-    </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} tleMode={tleMode} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route
+              path="/about"
+              element={
+                <TextForm
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
